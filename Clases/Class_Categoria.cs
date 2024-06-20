affc172d79b03a631d26bdc0e9484c6cad071da3
SilviaHG <sahg1997@gmail.com>
2024-06-20 13:38:19 -0600

    Primeras 7 clases

---
+++ Clases/Class_Categoria.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiendaPaula.Clases
+{
+    public class Class_Categoria
+    {
+        public Class_Categoria() { }
+
+        public int Id_Categoria { get; set; }
+        public string Categoria { get; set; }
+
+        public Class_Categoria(int id_Categoria, string categoria)
+        {
+            Id_Categoria = id_Categoria;
+            Categoria = categoria;
+        }
+
+    }
+}
