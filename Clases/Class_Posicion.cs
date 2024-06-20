e0d48b7f141807ca8d2cfb188376c33f01d3e95e
Jenni2859 <jenifer.garcia.2859@gmail.com>
2024-06-20 13:55:04 -0600

    Clases

---
+++ Clases/Class_Posicion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiendaPaula.Clases
+{
+    public class Class_Posicion
+    {
+
+        public int Id_Posicion { get; set; }
+
+        public String Nombre_Posicion { get; set; }
+
+        //Constructor vacio
+        public Class_Posicion() { }
+
+        // Constructor con parametros
+        public Class_Posicion(int id_Posicion, string nombre_Posicion)
+        {
+            Id_Posicion = id_Posicion;
+            Nombre_Posicion = nombre_Posicion;
+        }
+
+    }
+}
