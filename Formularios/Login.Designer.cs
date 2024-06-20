8685d7e9521c90b1c6a08dbfff8d57957a41ea44
SilviaHG <sahg1997@gmail.com>
2024-06-20 10:28:23 -0600

    Agregar archivos de proyecto.

---
+++ Formularios/Login.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace TiendaPaula.Formularios
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.linkCreateAccount = new System.Windows.Forms.LinkLabel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkForgotPass = new System.Windows.Forms.LinkLabel();
+            this.CheckboxRemember = new MaterialSkin.Controls.MaterialCheckbox();
+            this.btnSignIn = new MaterialSkin.Controls.MaterialButton();
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            this.txtPassword = new MaterialSkin.Controls.MaterialTextBox2();
+            this.txtUser = new MaterialSkin.Controls.MaterialTextBox2();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // linkCreateAccount
+            // 
+            this.linkCreateAccount.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.linkCreateAccount.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkCreateAccount.LinkColor = System.Drawing.Color.DarkCyan;
+            this.linkCreateAccount.Location = new System.Drawing.Point(292, 499);
+            this.linkCreateAccount.Name = "linkCreateAccount";
+            this.linkCreateAccount.Size = new System.Drawing.Size(118, 23);
+            this.linkCreateAccount.TabIndex = 31;
+            this.linkCreateAccount.TabStop = true;
+            this.linkCreateAccount.Text = "Create Account";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(180, 498);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 23);
+            this.label3.TabIndex = 30;
+            this.label3.Text = "Not a Member?";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label2.Location = new System.Drawing.Point(60, 489);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(468, 47);
+            this.label2.TabIndex = 29;
+            // 
+            // linkForgotPass
+            // 
+            this.linkForgotPass.AutoSize = true;
+            this.linkForgotPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkForgotPass.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.linkForgotPass.LinkColor = System.Drawing.Color.DarkCyan;
+            this.linkForgotPass.Location = new System.Drawing.Point(306, 415);
+            this.linkForgotPass.Name = "linkForgotPass";
+            this.linkForgotPass.Size = new System.Drawing.Size(138, 20);
+            this.linkForgotPass.TabIndex = 28;
+            this.linkForgotPass.TabStop = true;
+            this.linkForgotPass.Text = "Forgot Password?";
+            // 
+            // CheckboxRemember
+            // 
+            this.CheckboxRemember.AutoSize = true;
+            this.CheckboxRemember.Depth = 0;
+            this.CheckboxRemember.Location = new System.Drawing.Point(136, 407);
+            this.CheckboxRemember.Margin = new System.Windows.Forms.Padding(0);
+            this.CheckboxRemember.MouseLocation = new System.Drawing.Point(-1, -1);
+            this.CheckboxRemember.MouseState = MaterialSkin.MouseState.HOVER;
+            this.CheckboxRemember.Name = "CheckboxRemember";
+            this.CheckboxRemember.ReadOnly = false;
+            this.CheckboxRemember.Ripple = true;
+            this.CheckboxRemember.Size = new System.Drawing.Size(137, 37);
+            this.CheckboxRemember.TabIndex = 27;
+            this.CheckboxRemember.Text = "Remember Me";
+            this.CheckboxRemember.UseVisualStyleBackColor = true;
+            // 
+            // btnSignIn
+            // 
+            this.btnSignIn.AutoSize = false;
+            this.btnSignIn.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnSignIn.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.btnSignIn.Depth = 0;
+            this.btnSignIn.HighEmphasis = true;
+            this.btnSignIn.Icon = null;
+            this.btnSignIn.Location = new System.Drawing.Point(136, 365);
+            this.btnSignIn.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnSignIn.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnSignIn.Name = "btnSignIn";
+            this.btnSignIn.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.btnSignIn.Size = new System.Drawing.Size(308, 36);
+            this.btnSignIn.TabIndex = 26;
+            this.btnSignIn.Text = "Sign In";
+            this.btnSignIn.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.btnSignIn.UseAccentColor = false;
+            this.btnSignIn.UseVisualStyleBackColor = true;
+            // 
+            // materialLabel1
+            // 
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.materialLabel1.Location = new System.Drawing.Point(275, 227);
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(50, 19);
+            this.materialLabel1.TabIndex = 25;
+            this.materialLabel1.Text = "Sign In";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.AnimateReadOnly = false;
+            this.txtPassword.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.txtPassword.CharacterCasing = System.Windows.Forms.CharacterCasing.Normal;
+            this.txtPassword.Depth = 0;
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.txtPassword.HideSelection = true;
+            this.txtPassword.Hint = "Password";
+            this.txtPassword.LeadingIcon = null;
+            this.txtPassword.Location = new System.Drawing.Point(136, 310);
+            this.txtPassword.MaxLength = 32767;
+            this.txtPassword.MouseState = MaterialSkin.MouseState.OUT;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.PrefixSuffixText = null;
+            this.txtPassword.ReadOnly = false;
+            this.txtPassword.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txtPassword.SelectedText = "";
+            this.txtPassword.SelectionLength = 0;
+            this.txtPassword.SelectionStart = 0;
+            this.txtPassword.ShortcutsEnabled = true;
+            this.txtPassword.Size = new System.Drawing.Size(308, 48);
+            this.txtPassword.TabIndex = 24;
+            this.txtPassword.TabStop = false;
+            this.txtPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.txtPassword.TrailingIcon = null;
+            this.txtPassword.UseSystemPasswordChar = false;
+            // 
+            // txtUser
+            // 
+            this.txtUser.AnimateReadOnly = false;
+            this.txtUser.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.txtUser.CharacterCasing = System.Windows.Forms.CharacterCasing.Normal;
+            this.txtUser.Depth = 0;
+            this.txtUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.txtUser.HideSelection = true;
+            this.txtUser.Hint = "User";
+            this.txtUser.LeadingIcon = null;
+            this.txtUser.Location = new System.Drawing.Point(136, 256);
+            this.txtUser.MaxLength = 32767;
+            this.txtUser.MouseState = MaterialSkin.MouseState.OUT;
+            this.txtUser.Name = "txtUser";
+            this.txtUser.PasswordChar = '\0';
+            this.txtUser.PrefixSuffixText = null;
+            this.txtUser.ReadOnly = false;
+            this.txtUser.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txtUser.SelectedText = "";
+            this.txtUser.SelectionLength = 0;
+            this.txtUser.SelectionStart = 0;
+            this.txtUser.ShortcutsEnabled = true;
+            this.txtUser.Size = new System.Drawing.Size(308, 48);
+            this.txtUser.TabIndex = 23;
+            this.txtUser.TabStop = false;
+            this.txtUser.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.txtUser.TrailingIcon = null;
+            this.txtUser.UseSystemPasswordChar = false;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(233, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 127);
+            this.label1.TabIndex = 22;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Image = global::TiendaPaula.Properties.Resources.icons8_usuario_masculino_en_círculo;
+            this.pictureBox1.Location = new System.Drawing.Point(252, 119);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(98, 83);
+            this.pictureBox1.TabIndex = 32;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 625);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.linkCreateAccount);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.linkForgotPass);
+            this.Controls.Add(this.CheckboxRemember);
+            this.Controls.Add(this.btnSignIn);
+            this.Controls.Add(this.materialLabel1);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.label1);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.LinkLabel linkCreateAccount;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel linkForgotPass;
+        private MaterialSkin.Controls.MaterialCheckbox CheckboxRemember;
+        private MaterialSkin.Controls.MaterialButton btnSignIn;
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private MaterialSkin.Controls.MaterialTextBox2 txtPassword;
+        public MaterialSkin.Controls.MaterialTextBox2 txtUser;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ Formularios/Login.cs
@@ -0,0 +1,42 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TiendaPaula.Formularios
+{
+    public partial class Login : MaterialForm
+    {
+        public Login()
+        {
+            InitializeComponent();
+
+            InitializeComponent();
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+
+            //vamos a configurar el color
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(
+            Color.FromArgb(242, 148, 32),
+            Color.FromArgb(45, 48, 71),
+            Color.FromArgb(255, 224, 92),//tiene queser parecido al primero
+            Color.FromArgb(45, 48, 71),
+            TextShade.WHITE);
+
+
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
